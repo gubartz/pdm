1a09e2f5b68c46acbd55d9c3057aa8de9b1dd3b4
Gustavo Bartz Guedes <gubartz@gmail.com>
2016-02-22 09:42:56 -0300

    Adicionando tratamento de evento por meio de listener no Exemplo01

--- Exemplo01/app/src/main/java/br/edu/ifsp/hto/exemplo01/MainActivity.java
+++ Exemplo01/app/src/main/java/br/edu/ifsp/hto/exemplo01/MainActivity.java
@@ -3,18 +3,29 @@ package br.edu.ifsp.hto.exemplo01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btBotao2 = (Button) findViewById(R.id.btBotao2);
+        btBotao2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickBtBotao(v);
+            }
+        });
+
     }
 
     //G: Método invocado quando o botão é clicado
     public void onClickBtBotao(View v){
         Toast.makeText(this, "Olá Android!", Toast.LENGTH_LONG).show();
     }
+
 }
