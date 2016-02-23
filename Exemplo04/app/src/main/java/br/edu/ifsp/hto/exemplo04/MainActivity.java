db73ce520fccd08d33a0994b6f8dcb674f50051d
Gustavo Bartz Guedes <gubartz@gmail.com>
2016-02-23 11:45:41 -0300

    Pequenas correções como espaçamentos e nomes duplicados no index.html

--- Exemplo04/app/src/main/java/br/edu/ifsp/hto/exemplo04/MainActivity.java
+++ Exemplo04/app/src/main/java/br/edu/ifsp/hto/exemplo04/MainActivity.java
@@ -1,11 +1,11 @@
 package br.edu.ifsp.hto.exemplo04;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 //Baseado no livro de Ricardo Lecheta
-
 public class MainActivity extends AppCompatActivity {
     protected static final String TAG = "AULA";
 
@@ -13,48 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, getClassName() + "onCreate() chamado: " + savedInstanceState);
+        Log.i(TAG, getClassName() + " onCreate() chamado: " + savedInstanceState);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, getClassName() + "onStart() chamado");
+        Log.i(TAG, getClassName() + " onStart() chamado");
     }
 
     @Override
     protected void onRestart(){
         super.onRestart();
-        Log.i(TAG, getClassName() + "onRestart() chamado");
+        Log.i(TAG, getClassName() + " onRestart() chamado");
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-        Log.i(TAG, getClassName() + "onResume() chamado");
+        Log.i(TAG, getClassName() + " onResume() chamado");
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Log.i(TAG, getClassName() + "onPause() chamado");
+        Log.i(TAG, getClassName() + " onPause() chamado");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.i(TAG, getClassName() + "onSaveInstanceState() chamado");
+        Log.i(TAG, getClassName() + " onSaveInstanceState() chamado");
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-        Log.i(TAG, getClassName() + "onStop() chamado");
+        Log.i(TAG, getClassName() + " onStop() chamado");
     }
 
     protected void onDestroy(){
         super.onDestroy();
-        Log.i(TAG, getClassName() + "onDestroy() chamado");
+        Log.i(TAG, getClassName() + " onDestroy() chamado");
     }
 
     private String getClassName(){
--- Exemplo05/app/src/main/java/br/edu/ifsp/hto/exemplo05/DebugActivity.java
+++ Exemplo05/app/src/main/java/br/edu/ifsp/hto/exemplo05/DebugActivity.java
@@ -11,48 +11,48 @@ public class DebugActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, getClassName() + "onCreate() chamado: " + savedInstanceState);
+        Log.i(TAG, getClassName() + " onCreate() chamado: " + savedInstanceState);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, getClassName() + "onStart() chamado");
+        Log.i(TAG, getClassName() + " onStart() chamado");
     }
 
     @Override
     protected void onRestart(){
         super.onRestart();
-        Log.i(TAG, getClassName() + "onRestart() chamado");
+        Log.i(TAG, getClassName() + " onRestart() chamado");
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-        Log.i(TAG, getClassName() + "onResume() chamado");
+        Log.i(TAG, getClassName() + " onResume() chamado");
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Log.i(TAG, getClassName() + "onPause() chamado");
+        Log.i(TAG, getClassName() + " onPause() chamado");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.i(TAG, getClassName() + "onSaveInstanceState() chamado");
+        Log.i(TAG, getClassName() + " onSaveInstanceState() chamado");
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-        Log.i(TAG, getClassName() + "onStop() chamado");
+        Log.i(TAG, getClassName() + " onStop() chamado");
     }
 
     protected void onDestroy(){
         super.onDestroy();
-        Log.i(TAG, getClassName() + "onDestroy() chamado");
+        Log.i(TAG, getClassName() + " onDestroy() chamado");
     }
 
     private String getClassName(){
