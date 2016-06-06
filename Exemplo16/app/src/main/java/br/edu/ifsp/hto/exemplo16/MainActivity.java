c12e68a84aa1b17d0127c1bdd473d58a6dc13c0c
Gustavo Bartz Guedes <gubartz@gmail.com>
2016-06-06 14:58:35 -0300

    Correção no Exemplo16

--- Exemplo16/app/src/main/java/br/edu/ifsp/hto/exemplo16/MainActivity.java
+++ Exemplo16/app/src/main/java/br/edu/ifsp/hto/exemplo16/MainActivity.java
@@ -7,9 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TableLayout;
-
-import br.edu.ifsp.hto.exemplo16.tabs.SlidingTabLayout;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
